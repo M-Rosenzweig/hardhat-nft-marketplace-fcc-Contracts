64cec38998c679288fb9cf3c9de2856dbbd470c2
Moshe <moisherosenzweig@gmail.com>
2023-01-16 12:02:56 -0500

    inital commit + comments

--- deploy/01-deploy-nft-marketplace.js
+++ deploy/01-deploy-nft-marketplace.js
@@ -2,16 +2,18 @@ const { network } = require("hardhat")
 const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+module.exports = async ({ getNamedAccounts, deployments }) => { // these two objects come from the hre the Hardhat Runtime envirement. (i think thats what it stands for.. could be slightly off.)
+    // getNamedAccounts allows us to access the signer / accounts[0] the account we are using to deploy the stuff. 
+    // deployments lets us actually deploy and log (plus other stuff but that is what we are doing here..)
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const waitBlockConfirmations = developmentChains.includes(network.name)
+    const waitBlockConfirmations = developmentChains.includes(network.name) // if its a testnet than we should do 6 / couple as stipulated in our helper-hardhat-config
         ? 1
         : VERIFICATION_BLOCK_CONFIRMATIONS
 
-    log("----------------------------------------------------")
-    const arguments = []
-    const nftMarketplace = await deploy("NftMarketplace", {
+    log("----------------------------------------------------") // letting us know we are starting the magic. 
+    const arguments = [] // arguments to deploy 
+    const nftMarketplace = await deploy("NftMarketplace", { // NftMarketplace - hardhat is smart enough to look into contracts for a contract with this name and it takes care of buisness
         from: deployer,
         args: arguments,
         log: true,
